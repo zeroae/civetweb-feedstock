c9d02914c3ae91aaeea40d46ebb637846c77931d
Patrick Sodré <psodre@gmail.com>
2020-12-02 01:12:10 -0500

    Build shared civetweb

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,25 +2,23 @@
 set -eu
 
 ### Create Makefiles
-cmake -S src \
-      -B build \
-      -DCMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH\
-      -DCMAKE_INSTALL_PREFIX=$PREFIX \
+cmake -S . \
+      -B output \
+      ${CMAKE_ARGS} \
+      -DCMAKE_PREFIX_PATH=${CMAKE_PREFIX_PATH}\
+      -DCMAKE_INSTALL_PREFIX=${PREFIX} \
       -DCMAKE_INSTALL_LIBDIR=lib \
       -DCMAKE_BUILD_TYPE=Release \
       -DBUILD_SHARED_LIBS=ON \
-      -DPORTABLE=ON \
       -DCIVETWEB_ENABLE_CXX=ON \
       -DCIVETWEB_ENABLE_WEBSOCKETS=ON
 
 ## Build
-cmake --build build -- -j${CPU_COUNT}
-
-## Test
-cmake --build build -- test
-
-### Install
-cmake --build build -- install
+cmake --build output -- -j${CPU_COUNT}
 
 ### Checking requires a recompile with DEBUG enabled
-# cmake --build build -- check
+$GCC unittest/cgi_test.c -o output/cgi_test.cgi
+CTEST_OUTPUT_ON_FAILURE=1 cmake --build output -- test
+
+### Install
+cmake --build output -- install
